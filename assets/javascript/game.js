6e1ceca18ad971038991603af30c7249cc63ff43
DasBarenz <42988309+DasBarenz@users.noreply.github.com>
2018-10-07 21:30:09 -0600

    basic HTML and pseudo coding in JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+
+//computer GENERATES random numbers for each gem, between 1-12
+
+//computer GENERATES random number for user goal number, between 19-120
+//the goal number is DISPLAYED on this screen
+
+//user selects gem ON CLICK
+
+//the gem's randomly generated number adds to VALUE field
+
+//when value is EQUAL to goal number then win number is increased
+    //and game is reset
+
+//if value exceeds the goal number then loss number is increased
+    //and game is reset
+
+
+
+
+//from random number generator, lottery number generator
+    // $("#random-button").on("click", function() {
+    //     //start with empty string
+    //     var randomNum = "";
+    //     //create a new number
+    //     for (var i = 1; i < 10; i++) {
+    //         var randomDigit = Math.floor(Math.random() * 10);
+    //         //add the new number to the randomNum string
+    //         randomNum += randomDigit;
+    //         //same as randomNum = randomNum + randomDigit
+    //         }
+    //         console.log(randomNum);
+    //         $("#random-number").prepend("<p>" + randomNum + "</p>");
+
+//this is how to change string numbers to integers
+    // num1 = parseInt(num1);
+    // num2 = parseInt(num2);
+
+//reset function from calculator game
+    // function resetCalculator () {
+        // operatorChosen = false;
+        // num1 = "";
+        // num2 = "";
+        // operator = "";
+        // total = 0;
+        
+//reset from calculator game to reset during the operations by pressing a button "clear"
+    //$(".clear").on("click", function() {
+        //resetCalculator();
+    //     })
+
+//reset from calculator game to start the game fresh
+//resetCalculator();
+    });
+
+//from JS calculator
+
