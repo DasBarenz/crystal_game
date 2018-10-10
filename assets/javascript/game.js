b02b59b5fedc2882544a12efc89ebf22cbf40e9a
DasBarenz <42988309+DasBarenz@users.noreply.github.com>
2018-10-09 22:08:34 -0600

    goal number done and displayed; working on gem random values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,20 @@
 $(document).ready(function() {
+//computer GENERATES random number for user goal number, between 19-120
+//the goal number is DISPLAYED on the screen
+var goalValue = Math.floor((Math.random() * 101) + 19);
+    console.log(goalValue);
+    $("#goalValue").text("Your goal value is: " + goalValue)
 
 //computer GENERATES random numbers for each gem, between 1-12
 
-//computer GENERATES random number for user goal number, between 19-120
-//the goal number is DISPLAYED on this screen
+var gemArray = []
 
-//user selects gem ON CLICK
+function generateGemValues () {
+    for (i = 0; i < 5; i++)
+    $(gemArray.push) = Math.floor(Math.random() * 12) + 1;
+    console.log(gemArray); ///stopping work, i think this is good code but does not console log, and needs to be activated (function call)
+    };
 
-//the gem's randomly generated number adds to VALUE field
-
-//when value is EQUAL to goal number then win number is increased
-    //and game is reset
-
-//if value exceeds the goal number then loss number is increased
-    //and game is reset
-
-
-
-
-//from random number generator, lottery number generator
-    // $("#random-button").on("click", function() {
-    //     //start with empty string
-    //     var randomNum = "";
-    //     //create a new number
-    //     for (var i = 1; i < 10; i++) {
-    //         var randomDigit = Math.floor(Math.random() * 10);
-    //         //add the new number to the randomNum string
-    //         randomNum += randomDigit;
-    //         //same as randomNum = randomNum + randomDigit
-    //         }
-    //         console.log(randomNum);
-    //         $("#random-number").prepend("<p>" + randomNum + "</p>");
-
-//this is how to change string numbers to integers
-    // num1 = parseInt(num1);
-    // num2 = parseInt(num2);
-
-//reset function from calculator game
-    // function resetCalculator () {
-        // operatorChosen = false;
-        // num1 = "";
-        // num2 = "";
-        // operator = "";
-        // total = 0;
         
 //reset from calculator game to reset during the operations by pressing a button "clear"
     //$(".clear").on("click", function() {
@@ -50,8 +22,7 @@ $(document).ready(function() {
     //     })
 
 //reset from calculator game to start the game fresh
-//resetCalculator();
-    });
+    //resetCalculator();
 
-//from JS calculator
 
+})
