9e3a7553476ca96da857b36f97f96b885e8fab76
DasBarenz <42988309+DasBarenz@users.noreply.github.com>
2018-10-14 16:56:22 -0600

    got the chaching working, and added game reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,48 @@
-$(document).ready(function() {
-var goalValue = 0;
-var counter = 0;
-var wins = 0;
-var losses = 0;
+$(document).ready(function () {
+  var goalValue = 0;
+  var counter = 0;
+  var wins = 0;
+  var losses = 0;
+  var gemArray = [];
 
-function generateGoalValue() {
+  function generateGoalValue() {
     goalValue = (Math.floor((Math.random() * 101) + 19));
     $("#goalValue").text("Your goal value is: " + goalValue)
-};
-generateGoalValue();
-console.log(goalValue);
+  };
 
-var gemArray = [];
+  function resetGame() {
+    gemArray = [];
+    generateGemValues();
+    generateGoalValue();
+    counter = 0;
+  };
 
-function generateGemValues() {
-    for (var i = 1; i < 5; i++) {
-    gemArray.push(Math.floor(Math.random() * 12) + 1);
-    }
-};
-generateGemValues();
-console.log(gemArray);
+  generateGoalValue();
 
 
-$("#emerald").attr("data-crystalvalue", gemArray[0]);
-$("#tourmaline").attr("data-crystalvalue", gemArray[1]);
-$("#ruby").attr("data-crystalvalue", gemArray[2]);
-$("#citrine").attr("data-crystalvalue", gemArray[3]);
+  console.log(goalValue);
 
 
-$(".gems").on("click", function() {
+
+  function generateGemValues() {
+    for (var i = 0; i < 4; i++) {
+      gemArray.push(Math.floor(Math.random() * 12) + 1);
+    };
+  };
+
+  
+  generateGemValues();
+
+  console.log(gemArray);
+
+
+  $("#emerald").attr("data-crystalvalue", gemArray[0]);
+  $("#tourmaline").attr("data-crystalvalue", gemArray[1]);
+  $("#ruby").attr("data-crystalvalue", gemArray[2]);
+  $("#citrine").attr("data-crystalvalue", gemArray[3]);
+
+
+  $(".gems").on("click", function () {
     var crystalValue = ($(this).attr("data-crystalvalue"));
     console.log(crystalValue);
     crystalValue = parseInt(crystalValue);
@@ -37,19 +51,13 @@ $(".gems").on("click", function() {
     if (counter === goalValue) {
       wins++;
       $(".wins").text("Wins: " + wins);
-        document.getElementById("#chaching").play();
-        play();
-        gemArray = [];
-        generateGemValues();
-        generateGoalValue();
-        counter = 0;
+      var audio = document.getElementById("myAudio");
+      audio.play();
+      resetGame()
     } else if (counter > goalValue) {
       losses++;
       $(".losses").text("Losses: " + losses);
-      gemArray = [];
-      generateGemValues();
-      generateGoalValue();
-      counter = 0;
+      resetGame()
     };
 
   });
